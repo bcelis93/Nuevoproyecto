f28fe83206a79320ecd1d861662b1fa8f709801a
brian celis <brian.celis@lbo-corp.lbo.com.ar>
2022-10-04 19:11:46 -0300

    agrego templates

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def mostrar_inicio(request):
-   return HttpResponse("Hola crack!")
+    return render(request, "AppCoder/inicio.html")
