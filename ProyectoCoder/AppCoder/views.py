877e243a87000fb05ea93cc3602b990cd356900a
brian celis <brian.celis@lbo-corp.lbo.com.ar>
2022-10-04 13:27:43 -0300

    agrego primera vista

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,7 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def mostrar_inicio(request):
+   return HttpResponse("Hola crack!")
