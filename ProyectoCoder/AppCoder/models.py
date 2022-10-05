a668e00d1ab87ada03d7d2192366b8a14d99da8a
brian celis <brian.celis@lbo-corp.lbo.com.ar>
2022-10-04 22:26:08 -0300

    famili completa

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Estudiante(models.Model):
+class Familia(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    email = models.EmailField()
+    edad = models.CharField(max_length=5)
+    nacimiento = models.DateField()
--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -2,9 +2,11 @@ from http.client import HTTPResponse
 from django.shortcuts import render
 
 
-from AppCoder.models import Estudiante
+from AppCoder.models import Familia
 
 def mostrar_inicio(request):
-    familia = Estudiante(nombre="Daniela", apellido="Celis", email="dani@gmail.com")
-    contexto = {"familia_1" : familia}
+    familia = Familia(nombre="Daniela", apellido="Celis", edad="21", nacimiento="1999-10-10")
+    familia_2 = Familia(nombre="Javier", apellido="Celis", edad="58", nacimiento="1964-05-08")
+    familia_3 = Familia(nombre="Luana", apellido="Celis", edad="33", nacimiento="1989-07-12")
+    contexto = {"familia_1" : familia, "familia_2" : familia_2}
     return render(request, "AppCoder/inicio.html", contexto)
