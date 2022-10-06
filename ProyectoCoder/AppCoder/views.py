ad369429f34e467d99d11d171ad28decd7e6c48e
brian celis <brian.celis@lbo-corp.lbo.com.ar>
2022-10-06 12:24:11 -0300

    base de datos

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -6,7 +6,11 @@ from AppCoder.models import Familia
 
 def mostrar_inicio(request):
     familia = Familia(nombre="Daniela", apellido="Rodriguez", edad="21", nacimiento="1999-10-10")
+    familia.save()
     familia_2 = Familia(nombre="Javier", apellido="Rodriguez", edad="58", nacimiento="1964-05-08")
+    familia_2.save()
     familia_3 = Familia(nombre="Luana", apellido="Rodriguez", edad="33", nacimiento="1989-07-12")
     contexto = {"familia_1" : familia, "familia_2" : familia_2, "familia_3" : familia_3}
+    familia_3.save()
+
     return render(request, "AppCoder/inicio.html", contexto)
